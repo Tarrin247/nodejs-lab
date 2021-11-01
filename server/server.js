c6c0d3214f88e76aafc29dd77c4df94eac4cb8d6
Timple Basinger <tbasinger@protonmail.com>
2021-11-01 13:21:26 -0500

    add .gitignore file

--- server/server.js
+++ server/server.js
@@ -2,8 +2,26 @@ var path = require('path');
 
 var fs = require('fs');
 
-
 var chirpsPath = (path.join(__dirname, '../chirps.json'));
+
+const chirps= [
+   { username: 'Bo',
+    chirp: "this doesn't feel like Kansas anymore",
+    
+    username: "Lilly",
+    chirp: "this doesn't feel like Spain anymore",
+    
+    username: "Grace",
+    chirp: "this doesn't feel like Walter anymore",
+    
+    username: "Marley",
+    chirp: "this doesn't feel like Austin anymore",
+   
+    username: "Macey",
+    chirp: "this doesn't feel like Fayette anymore"
+},
+];
+
 
 fs.readFile(chirpsPath, {
     encoding: "UTF-8"
